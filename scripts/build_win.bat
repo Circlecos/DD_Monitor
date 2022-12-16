d4035f97b234b92305cee455344c241bcd495f78
Circlecos <Circlecos@users.noreply.github.com>
2022-12-16 13:16:00 +0000

    fix: actions

--- scripts/build_win.bat
+++ scripts/build_win.bat
@@ -6,24 +6,24 @@ mkdir dist\DDMonitor\logs
 copy utils\config_default.json dist\DDMonitor\utils\config.json
 
 rem remove useless dll
-del /F /Q dist\DDMonitor\Qt5DBus.dll
-del /F /Q dist\DDMonitor\Qt5Network.dll
-del /F /Q dist\DDMonitor\Qt5Qml.dll
-del /F /Q dist\DDMonitor\Qt5QmlModels.dll
-del /F /Q dist\DDMonitor\Qt5Quick.dll
-del /F /Q dist\DDMonitor\Qt5Svg.dll
-del /F /Q dist\DDMonitor\Qt5WebSockets.dll
-
-del /F /Q dist\DDMonitor\libEGL.dll
-del /F /Q dist\DDMonitor\libGLESv2.dll
-
-del /F /Q dist\DDMonitor\opengl32sw.dll
-del /F /Q dist\DDMonitor\ucrtbase.dll
-
-del /F /Q dist\DDMonitor\pyexpat.pyd
-del /F /Q dist\DDMonitor\_decimal.pyd
-del /F /Q dist\DDMonitor\_multiprocessing.pyd
-
-rem Remove dir
-RMDIR /S /Q dist\DDMonitor\PyQt5\Qt\translations
-RMDIR /S /Q dist\DDMonitor\Include
+rem del /F /Q dist\DDMonitor\Qt5DBus.dll
+rem del /F /Q dist\DDMonitor\Qt5Network.dll
+rem del /F /Q dist\DDMonitor\Qt5Qml.dll
+rem del /F /Q dist\DDMonitor\Qt5QmlModels.dll
+rem del /F /Q dist\DDMonitor\Qt5Quick.dll
+rem del /F /Q dist\DDMonitor\Qt5Svg.dll
+rem del /F /Q dist\DDMonitor\Qt5WebSockets.dll
+rem
+rem del /F /Q dist\DDMonitor\libEGL.dll
+rem del /F /Q dist\DDMonitor\libGLESv2.dll
+rem
+rem del /F /Q dist\DDMonitor\opengl32sw.dll
+rem del /F /Q dist\DDMonitor\ucrtbase.dll
+rem
+rem del /F /Q dist\DDMonitor\pyexpat.pyd
+rem del /F /Q dist\DDMonitor\_decimal.pyd
+rem del /F /Q dist\DDMonitor\_multiprocessing.pyd
+rem
+rem rem Remove dir
+rem RMDIR /S /Q dist\DDMonitor\PyQt5\Qt\translations
+rem RMDIR /S /Q dist\DDMonitor\Include
